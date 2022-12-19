5cec281acbf189b7762bb7a3371bbfaaf48c6f7d
Benjamin Brock <benjamin.brock@intel.com>
2022-12-19 11:43:35 -0500

    Add writing

--- examples/simple_io.cpp
+++ examples/simple_io.cpp
@@ -2,12 +2,13 @@
 #include <binsparse/binsparse.hpp>
 
 int main(int argc, char** argv) {
-  printf("Hello, world!\n");
   auto mat = binsparse::read_coo_matrix<float>("data/matrix.hdf5");
 
   for (size_t i = 0; i < mat.nnz; i++) {
     std::cout << mat.rowind[i] << ", " << mat.colind[i] << ": " << mat.values[i] << std::endl;
   }
+
+  binsparse::write_coo_matrix("new_matrix.hdf5", mat);
 
   return 0;
 }
--- include/binsparse/binsparse.hpp
+++ include/binsparse/binsparse.hpp
@@ -8,69 +8,33 @@
 
 namespace binsparse {
 
-/*
 template <typename T, typename I>
-void write_matrix(std::string fname,
-                  const std::vector<std::tuple<std::tuple<I, I>, T>>& tuples) {
-
-  using value_type = T
-  using index_type = I
-
-  std::vector<std::ranges::range_value_t<M>> tuples(matrix.begin(), matrix.end());
-
-  auto sort_fn = [](const auto& a, const auto& b) {
-                   auto&& [a_index, a_value] = a;
-                   auto&& [b_index, b_value] = b;
-                   auto&& [a_i, a_j] = a_index;
-                   auto&& [b_i, b_j] = b_index;
-                   if (a_i < b_i) {
-                     return true;
-                   }
-                   else if (a_i == b_i) {
-                     if (a_j < b_j) {
-                      return true;
-                     }
-                   }
-                   return false;
-                 };
-
-  std::sort(tuples.begin(), tuples.end(), sort_fn);
-
-  std::vector<grb::matrix_index_t<M>> rows;
-  std::vector<grb::matrix_index_t<M>> cols;
-  std::vector<grb::matrix_scalar_t<M>> vals;
-
-  rows.reserve(matrix.size());
-  cols.reserve(matrix.size());
-  vals.reserve(matrix.size());
-
-  for (auto&& [index, value] : tuples) {
-    auto&& [row, col] = index;
-    rows.push_back(row);
-    cols.push_back(col);
-    vals.push_back(value);
-  }
+void write_coo_matrix(std::string fname,
+                      coo_matrix<T, I> m) {
 
   H5::H5File f(fname.c_str(), H5F_ACC_TRUNC);
 
-  hdf5_tools::write_dataset(f, "values", vals);
-  hdf5_tools::write_dataset(f, "indices_0", rows);
-  hdf5_tools::write_dataset(f, "indices_1", cols);
+  std::span<T> values(m.values, m.nnz);
+  std::span<I> rowind(m.rowind, m.nnz);
+  std::span<I> colind(m.colind, m.nnz);
+
+  hdf5_tools::write_dataset(f, "values", values);
+  hdf5_tools::write_dataset(f, "indices_0", rowind);
+  hdf5_tools::write_dataset(f, "indices_1", colind);
 
   std::string json_string =
   "{\n"
   "  \"format\": \"COO\",\n"
   "  \"shape\": [";
-  json_string += std::to_string(matrix.shape()[0]) + ", " + std::to_string(matrix.shape()[1]) +
+  json_string += std::to_string(m.m) + ", " + std::to_string(m.n) +
   "],\n" +
-  "  \"nnz\": " + std::to_string(matrix.size()) + "\n" +
+  "  \"nnz\": " + std::to_string(m.nnz) + "\n" +
   "}\n";
 
   hdf5_tools::write_dataset(f, "metadata", json_string);
 
   f.close();
 }
-*/
 
 template <typename T, typename I, typename Allocator>
 coo_matrix<T, I> read_coo_matrix(std::string fname, Allocator&& alloc) {
